4700fc98f937a9949c360a8a12e2ed6bc055fd65
CynicznyKot <IotaJot@gmail.com>
2026-08-31 20:33:57 +0500

    feat: add Windows installation script

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo
+echo Installing DevTimeTracker on Windows...
+echo ========================================
+
+echo Installing dependencies...
+pip install -r requirements.txt
+
+echo Done!
+pause
